57bb0a2725a0b22c42380f6ab39e40ab67755f11
VictorClapzPalma <106077684+VictorClapzPalma@users.noreply.github.com>
2023-11-30 08:51:07 -0300

    a

---
+++ oficial/src/components/Erro/erro.js
@@ -0,0 +1,12 @@
+import Header from "../Header/header"
+
+export default function Erro(){
+    return(
+        <>
+        <Header/>
+        <h1>
+        Erro 404
+        </h1>
+        </>
+    )
+}
