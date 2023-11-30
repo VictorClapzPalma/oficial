e103072d119619e030ed77b9a8bbbefac9c8c3ab
VictorClapzPalma <106077684+VictorClapzPalma@users.noreply.github.com>
2023-11-30 09:04:28 -0300

    a

---
+++ oficial/src/components/Right/right.js
@@ -0,0 +1,21 @@
+import "./right.scss"
+
+export default function Right() {
+
+    return(
+        <section class="right">
+        <a class="name" href="/registrar">
+                    INSERIR
+                </a>
+                <a class="name" href="/alterar">
+                    ALTERAR
+                </a>
+                <a class="name" href="/excluir">
+                    REMOVER
+                </a>
+                <a class="name" href="/listar">
+                    LISTAR
+                </a>
+        </section>
+    )
+}
